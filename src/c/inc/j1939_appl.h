75ce5a5b35a884500371d202e1bd34f0607c8bef
tomKimsey <thk@hms.se>
2019-04-04 11:18:21 -0400

    Added read functionality to j1939_appl
    j1939_appl will now read messages from the j1939 bus.  If a
    message is received it will be placed into the received message
    array.

--- src/c/inc/j1939_appl.h
+++ src/c/inc/j1939_appl.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "edgex/devsdk.h"
+#include "XatJ1939.h"
+#include "XatJ1939_err.h"
+#include "XatJ1939_types.h"
 
 #define J1939_APPL_NO_ERROR 0
 #define J1939_APPL_ERROR -1
@@ -24,3 +27,12 @@ int j1939_appl_init(bool isApplCtlIfr, iot_logging_client *lc);
  * @return 0 upon success, non-zero upon error
  */
 int j1939_appl_stop(void);
+
+/**
+ * Get the last message received for a given PGN
+ * @param uint32_t pgn - PGN to be looked up
+ * @param J1939API_J1939MSG **msg - pointer to pointer
+ * to received J1939 message
+ * @return 0 upon success, non-zero upon error
+ */
+int J1939_Get_Rec_Msg(uint32_t pgn, J1939API_J1939MSG **msg);
--- src/c/j1939_appl.c
+++ src/c/j1939_appl.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "j1939_appl.h"
-#include "XatJ1939.h"
-#include "XatJ1939_err.h"
-#include "XatJ1939_types.h"
 #include "libsocketcan.h"
 
 #define CAN_IFR "can0"
 #define BITRATE 250000
+#define MAX_NUM_PGNS 250
 
 //Function Prototypes
 static int ConfigureCan(const char *interface, uint32_t bitrate);
 static int ShutdownCan(const char *interface);
+void *readThread(void *arg);
 
 //"SocketCan"
 static char sBoardtype[16] = {0x53, 0x6f, 0x63, 0x6b, 0x65, 0x74, 0x43, 0x41, 0x4e, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -19,9 +19,12 @@ static char sBoardtype[16] = {0x53, 0x6f, 0x63, 0x6b, 0x65, 0x74, 0x43, 0x41, 0x
 static char sBoardID[16] = {0x63, 0x61, 0x6e, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 static bool isJ1939ApplRunning = false;
+static bool j1939RecThreadShouldRun = false;
 static bool isApplControlInterface = false;
 static UINT8 j1939StackInst = 0;
 static iot_logging_client *loggingClient;
+static J1939API_J1939MSG received_msgs[MAX_NUM_PGNS];
+static pthread_t thread_id;
 
 /**
  * Creates J1939 stack instance and brings up
@@ -42,6 +45,8 @@ int j1939_appl_init(bool isInterfaceControl, iot_logging_client *lc)
       isApplControlInterface = isInterfaceControl;
       loggingClient = lc;
 
+      memset(&received_msgs, 0, sizeof received_msgs);
+
       if (isApplControlInterface)
          retval = ConfigureCan(CAN_IFR, BITRATE);
 
@@ -52,6 +57,8 @@ int j1939_appl_init(bool isInterfaceControl, iot_logging_client *lc)
          if (retval == J1939_APPL_NO_ERROR)
          {
             iot_log_debug(loggingClient, "J1939 stack instance created");
+            j1939RecThreadShouldRun = true;
+            pthread_create(&thread_id, NULL, readThread, &j1939RecThreadShouldRun);
          }
          else
          {
@@ -83,7 +90,10 @@ int j1939_appl_init(bool isInterfaceControl, iot_logging_client *lc)
  */
 int j1939_appl_stop(void)
 {
+   void *res;
    int retval = J1939_APPL_NO_ERROR;
+
+   j1939RecThreadShouldRun = false;
 
    if (isApplControlInterface)
       retval = ShutdownCan(CAN_IFR);
@@ -98,6 +108,8 @@ int j1939_appl_stop(void)
       else
       {
          isJ1939ApplRunning = false;
+         pthread_join(thread_id, &res);
+         free(res);
       }
    }
    else
@@ -106,6 +118,81 @@ int j1939_appl_stop(void)
    }
 
    return retval;
+}
+
+/**
+ * Get the last message received for a given PGN
+ * @param uint32_t pgn - PGN to be looked up
+ * @param J1939API_J1939MSG **msg - pointer to pointer
+ * to received J1939 message
+ * @return 0 upon success, non-zero upon error
+ */
+int J1939_Get_Rec_Msg(uint32_t pgn, J1939API_J1939MSG **msg)
+{
+   for (int pgnIndex = 0; pgnIndex < MAX_NUM_PGNS; pgnIndex++)
+   {
+      if (received_msgs[pgnIndex].dwPgn == pgn)
+      {
+         *msg = &received_msgs[pgnIndex];
+         return J1939_APPL_NO_ERROR;
+      }
+   }
+   iot_log_warning(loggingClient, "Message for PGN %d not found", pgn);
+   return J1939_APPL_ERROR;
+}
+
+/**
+ * Thread to read data from the J1939 bus
+ */
+void *readThread(void *arg)
+{
+   bool *threadShouldRun = arg;
+   while (*threadShouldRun)
+   {
+      J1939API_J1939MSG newmsg;
+      int res = J1939Api_ReceiveMessage(j1939StackInst, (char *)&newmsg, sizeof(newmsg));
+      if (res == J1939API_S_OK)
+      {
+         bool existingMsgFound = false;
+         bool msgStored = false;
+
+         //Check for an existing message for this PGN
+         for (int pgnIndex = 0; pgnIndex < MAX_NUM_PGNS; pgnIndex++)
+         {
+            if (received_msgs[pgnIndex].dwPgn == newmsg.dwPgn)
+            {
+               received_msgs[pgnIndex] = newmsg;
+               existingMsgFound = true;
+               msgStored = true;
+               break;
+            }
+         }
+
+         //No previous message for this PGN was found
+         if (!existingMsgFound)
+         {
+            for (int pgnIndex = 0; pgnIndex < MAX_NUM_PGNS; pgnIndex++)
+            {
+               //Find an empty index
+               if (received_msgs[pgnIndex].dwPgn == 0)
+               {
+                  received_msgs[pgnIndex] = newmsg;
+                  msgStored = true;
+                  break;
+               }
+            }
+         }
+
+         if (!msgStored)
+            iot_log_error(loggingClient, "Could not store message for PGN: %d", newmsg.dwPgn);
+      }
+      //No message was received, sleep the thread to reduce load
+      else if (res == J1939API_E_MSG_RX_TIMEOUT)
+      {
+         usleep(10);
+      }
+   }
+   return NULL;
 }
 
 /**
